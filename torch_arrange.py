1aeedf5d74ac077ba3e66ea8dbb66d49b0ea10a8
Tae-Kyu Hong <48700490+YnHong@users.noreply.github.com>
2019-04-04 12:20:32 +0900

    Add files via upload

---
+++ torch_arrange.py
@@ -0,0 +1,117 @@
+from PIL import Image as pil
+import numpy as np
+import pandas as pd
+from IPython.core.debugger import set_trace
+
+# %matplotlib inline
+# %config InlineBackend.figure_formats = {'png', 'retina'}
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+import torch.nn.functional as F
+from keras.preprocessing.image import ImageDataGenerator
+
+import pickle
+
+# from torch import random_weight, zero_weight, flatten, Flatten, check_accuracy, train
+
+def random_weight(shape):
+    """
+    Create random Tensors for weights; setting requires_grad=True means that we
+    want to compute gradients for these Tensors during the backward pass.
+    We use Kaiming normalization: sqrt(2 / fan_in)
+    """
+    if len(shape) == 2:  # FC weight
+        fan_in = shape[0]
+    else:
+        fan_in = np.prod(shape[1:]) # conv weight [out_channel, in_channel, kH, kW]
+    # randn is standard normal distribution generator.
+    w = torch.randn(shape, device=device, dtype=dtype) * np.sqrt(2. / fan_in)
+    w.requires_grad = True
+    return w
+
+def zero_weight(shape):
+    return torch.zeros(shape, device=device, dtype=dtype, requires_grad=True)
+
+# create a weight of shape [3 x 5]
+# you should see the type `torch.cuda.FloatTensor` if you use GPU.
+# Otherwise it should be `torch.FloatTensor`
+
+def flatten(x):
+    '''input : x --> return flattened x'''
+    N = x.shape[0]
+    return x.view(N,-1)
+
+class Flatten(nn.Module):
+    def forward(self,x):
+        return flatten(x)
+
+def check_accuracy(loader, model, data = 'val'):
+    if data == 'val':
+        print('Checking accuracy on validation set')
+    else:
+        print('Checking accuracy on test set')
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+        acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+
+
+def train(model, optimizer, device, epochs=1,):
+    """
+    Train a model on CIFAR-10 using the PyTorch Module API.
+
+    Inputs:
+    - model: A PyTorch Module giving the model to train.
+    - optimizer: An Optimizer object we will use to train the model
+    - epochs: (Optional) A Python integer giving the number of epochs to train for
+
+    Returns: Nothing, but prints model accuracies during training.
+    """
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for e in range(epochs):
+        for t, (x, y) in enumerate(train_loader):
+            model.train()  # put model to training mode
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+#             print(scores)
+#             print(y)
+            loss = F.cross_entropy(scores, y)
+
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+            optimizer.zero_grad()
+
+            # This is the backwards pass: compute the gradient of the loss with
+            # respect to each  parameter of the model.
+            loss.backward()
+
+            # Actually update the parameters of the model using the gradients
+            # computed by the backwards pass.
+            optimizer.step()
+
+            if t % print_every == 0:
+                print('Iteration %d, loss = %.4f' % (t, loss.item()))
+                check_accuracy(val_loader, model)
+                print()
